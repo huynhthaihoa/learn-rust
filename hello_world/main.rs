0e851a0ca3d16e809e063baca3b6588a4987f913
Huynh Thai Hoa <huynhlequang@gmail.com>
2022-12-17 23:06:27 +0900

    Initial commit

---
+++ hello_world/main.rs
@@ -0,0 +1,8 @@
+// entry point
+fn main()
+{
+    //call a Rust macro
+    //If it had called a function instead, it would be entered as println (without the !)
+    //"Hello World!" is an argument to println!
+    println! ("Hello World!");
+}
